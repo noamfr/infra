557d615f86621c15ca6e431a5c9b2b98a0779006
noamfr <noam.frank1@gmail.com>
2020-09-15 23:48:33 +0300

    added bar chart function

--- analysis_operations/graph_functions.py
+++ analysis_operations/graph_functions.py
@@ -3,12 +3,12 @@ import os
 from matplotlib import pyplot as plt
 
 
-def hist(path: str,
-         vector: np.ndarray,
-         label: str,
-         x_label: str,
-         y_label: str,
-         add_mean_line: bool = False):
+def histogram(path: str,
+              vector: np.ndarray,
+              label: str,
+              x_label: str,
+              y_label: str,
+              add_mean_line: bool = False):
 
     plt.clf()
     plt.hist(vector, label=label, color='royalblue')
@@ -24,3 +24,15 @@ def hist(path: str,
     plt.legend(loc='best', shadow=True)
     plt.tight_layout()
     plt.savefig(os.path.join(path, f'{label}_histogram.png'))
+
+
+def bar_chart(x, height, title: str, x_label: str, y_label: str, path: str):
+    plt.clf()
+    plt.bar(x=x, height=height)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.xticks(rotation=45, ha='right', fontsize=8, weight='bold')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(os.path.join(path, f'{title}_bar_chart.png'))
