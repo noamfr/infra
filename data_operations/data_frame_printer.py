68493ae051d5254635fb285c625059e0a4e2ba46
noamfr <noam.frank1@gmail.com>
2020-10-17 23:38:55 +0300

    minor change to class

--- data_operations/data_frame_printer.py
+++ data_operations/data_frame_printer.py
@@ -9,4 +9,4 @@ class Data_Frame_Printer:
 
     def print_df_from_dict(self, default_dict: DefaultDict, file_name: str):
         df = pd.DataFrame(default_dict)
-        df.to_csv(os.path.join(self.path, file_name))
+        df.to_csv(os.path.join(self.path, f'{file_name}.csv'))
