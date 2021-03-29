fb331b7329eea0741636b75851ea6192c782128c
noamfr <noam.frank1@gmail.com>
2021-03-29 21:53:47 +0300

    update to pipeline_executor

--- pipeline_operations/pipeline_executor.py
+++ pipeline_operations/pipeline_executor.py
@@ -6,18 +6,31 @@ from .timer import Timer
 
 
 class Pipeline_Executor:
-    def __init__(self, pipeline_class, steps: List, save_history: bool, save_state: bool, pickle_path: str,
+    def __init__(self, pipeline_class, steps: List, delete_history: bool, save_state: bool, pickle_path: str,
                  job_key: str):
         self.pipeline = pipeline_class
         self.steps = steps
-        self.save_history = save_history
+        self.delete_history = delete_history
         self.save_state = save_state
         self.pickle_path = pickle_path
         self.job_key = job_key
 
     def execute(self):
-        self.__load_pipeline()
+        self.__load_pipeline_if_needed()
         self.__run_steps()
+
+    def __load_pipeline_if_needed(self):
+        logging.info("================ Loading pickle file ===========")
+        print("================ Loading pickle file ===========")
+
+        if not self.delete_history:
+
+            try:
+                file = open(path.join(self.pickle_path, self.job_key + '.pickle'), 'rb')
+                self.pipeline = pickle.load(file)
+
+            except FileNotFoundError:
+                raise FileNotFoundError
 
     def __run_steps(self):
         for step in self.steps:
@@ -40,14 +53,3 @@ class Pipeline_Executor:
             print("================ Saving: " + step + " to pickle ================")
             with open(path.join(self.pickle_path, self.job_key + '.pickle'), 'wb') as handle:
                 pickle.dump(obj=self.pipeline, file=handle)
-
-    def __load_pipeline(self):
-        logging.info("================ Loading pickle file ===========")
-        print("================ Loading pickle file ===========")
-
-        if self.save_history:
-            try:
-                file = open(path.join(self.pickle_path, self.job_key + '.pickle'), 'rb')
-                self.pipeline = pickle.load(file)
-            except:
-                pass
