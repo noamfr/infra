245c6d4f5165404695581b888c66936b5ace9ef0
noamfr <noam.frank1@gmail.com>
2021-07-03 20:42:53 +0300

    added line chart and distribution plot

--- analysis_operations/graph_functions.py
+++ analysis_operations/graph_functions.py
@@ -48,6 +48,18 @@ def bar_chart(x, height, title: str, x_label: str, y_label: str, path: str):
     plt.savefig(os.path.join(path, f'{title}_bar_chart.png'), dpi=500)
 
 
+def lines_chart(data: pd.DataFrame, x: str, y: str, hue=None, style=None):
+    plt.figure(figsize=(25, 12))
+    sns.lineplot(x=x, y=y, data=data, hue=hue, style=style, markers=True, legend='full')
+    plt.legend(fontsize=18, shadow=True)
+    plt.xticks(np.arange(0, data[x].nunique(), 1), fontsize=16, fontweight='bold')
+    plt.yticks(fontsize=14, fontweight='bold')
+    plt.xlabel(x, fontsize=18, fontweight='bold')
+    plt.ylabel(y, fontsize=18, fontweight='bold')
+    plt.title(f'{hue} by {y}', fontsize=24, fontweight='bold')
+    plt.show()
+
+
 def correlation_matrix(
         corr: pd.DataFrame,
         labels: List[str],
@@ -66,33 +78,33 @@ def correlation_matrix(
     plt.savefig(os.path.join(output_path, f'{title}.png'))
 
 
-def distribution_analysis(
-        output_path: str,
-        data: pd.DataFrame,
-        feature_name: str):
-
+def distribution_analysis(data: pd.DataFrame, feature_name: str, label: str):
     vec = data[feature_name]
     mini = vec.min()
     maxi = vec.max()
     rng = maxi - mini
     mean = vec.mean()
     median = vec.median()
+    mode = vec.mode()[0]
     std = vec.std()
     skew = vec.skew()
     kurt = vec.kurtosis()
     points = mean - std, mean + std
+    n = len(vec)
 
+    plt.clf()
+    plt.figure(figsize=(12, 6))
     sns.distplot(vec, hist=True, kde=True)
-    sns.lineplot(points, [0, 0], color='black', label="std_dev")
-    sns.scatterplot([mini, maxi], [0, 0], color='orange', label="min/max")
-    sns.scatterplot([mean], [0], color='red', label="mean")
-    sns.scatterplot([median], [0], color='blue', label="median")
+    sns.lineplot(points, [0, 0], color='black', label=f'std_dev: {round(std,2)}')
+    sns.scatterplot([mini, maxi], [0, 0], color='orange', label=f"min/max{mini, maxi}", s=100, alpha=1)
+    sns.scatterplot([mean], [0], color='red', label=f'mean: {round(mean,2)}', s=100, alpha=1)
+    sns.scatterplot([median], [0], color='blue', label=f'median: {round(median, 2)}', s=100, alpha=1)
+    sns.scatterplot([mode], [0], color='green', label=f'mode: {mode}', s=100, alpha=1)
 
-    plt.xlabel(f'{feature_name}', fontsize=20)
-    plt.ylabel('density')
-    plt.title(f'std_dev = {round(std, 2)}; kurtosis = {round(kurt,2)};\nskew = {round(skew, 2)};range = {round(rng, 2)}\nmean = {round(mean, 2)}; median = {round(median, 2)}')
-    plt.savefig(os.path.join(output_path, f'{feature_name}_distribution.png'))
-
-
-
+    plt.xlabel(f'{feature_name}', fontsize=16)
+    plt.ylabel('density', fontsize=16)
+    plt.title(f'Distribution of {feature_name} for {label.upper()} (n={n})\nskew = {round(skew, 2)}; kurtosis = {round(kurt, 2)}', fontsize=18)
+    plt.legend(fontsize=14, shadow=True, frameon=True)
+    plt.tight_layout()
+    plt.show()
 
